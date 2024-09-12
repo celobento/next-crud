81590ea4ee622275b017760efe0f9de931b5ed78
Marcelo Bento <celobento26@gmail.com>
2024-09-11 21:50:42 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div
+      className={`flex justify-center items-center
+                  h-screen`}
+    >
+      <span>CRUD Project</span>
+    </div>
+  );
+}
